bccfe2dbe71bf6ffb6390c3c63c7dafbc479ffd1
chris241 <chrisleon325@gmail.com>
2020-12-26 12:40:35 +0300

    projet winform

---
+++ testTreeFrom/TestTree/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace TestTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.initialiser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(24, 12);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(764, 343);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            title1.Name = "Affichage de Graphe";
+            this.chart.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(195, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // initialiser
+            // 
+            this.initialiser.Location = new System.Drawing.Point(360, 387);
+            this.initialiser.Name = "initialiser";
+            this.initialiser.Size = new System.Drawing.Size(91, 36);
+            this.initialiser.TabIndex = 2;
+            this.initialiser.Text = "intialiser";
+            this.initialiser.UseVisualStyleBackColor = true;
+            this.initialiser.Click += new System.EventHandler(this.initialiser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.initialiser);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart);
+            this.Name = "Form1";
+            this.Text = "Graphe";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button initialiser;
+    }
+}
+
---
+++ testTreeFrom/TestTree/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using testTree.Model;
+
+namespace TestTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_ClickAsync(object sender, EventArgs e)
+        {
+            chart.Series["Series1"].Points.Clear();
+            try
+            {           
+                string uri = "http://localhost:44327/api/tree";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)";
+                request.Accept = "/";
+                string result;
+                List<Synthese> json = null; 
+                HttpWebResponse tests = request.GetResponse() as HttpWebResponse;
+                using (StreamReader r = new StreamReader(tests.GetResponseStream()))
+                {
+                    result  = r.ReadToEnd();
+                    json = JsonConvert.DeserializeObject<List<Synthese>>(result);
+                }
+                if ( tests != null)
+                {
+                    foreach (Synthese test in json)
+                    {
+                        chart.Series["Series1"].Points.AddXY(test.Categorie,test.Pourcentage.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("il n'y a pas de données pour le moment");
+                }
+               
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void initialiser_Click(object sender, EventArgs e)
+        {
+            chart.Series["Series1"].Points.Clear();
+        }
+    }
+}
